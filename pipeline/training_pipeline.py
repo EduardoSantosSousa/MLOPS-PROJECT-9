125333d8ce856bcdeeede03b61baa5016d9b33dc
Eduardo Sousa <eduardosousa.eds@gmail.com>
2025-07-07 20:37:34 -0300

    Commit

--- pipeline/training_pipeline.py
+++ pipeline/training_pipeline.py
@@ -7,10 +7,27 @@ from src.model_training import ModelTraining
 if __name__ == "__main__":
     config = read_yaml(file_path=CONFIG_PATH)
 
+    #azure_params = {
+    #    "connection_string": config['azure_storage_account_config']['connection_string'],
+    #    "container_name": config['azure_storage_account_config']['container_name'],
+    #    "blob_name": config['azure_storage_account_config']['blob_name'],
+    #}
+
+    # lê secrets primeiro, senão cai no YAML
+    connection_string = os.getenv("CONNECTION_STRING")
+    container_name    = os.getenv("CONTAINER_NAME")
+    blob_name         = os.getenv("BLOB_NAME")
+    
+    if not (connection_string and container_name and blob_name):
+        config = read_yaml(file_path=CONFIG_PATH)
+        connection_string = connection_string or config['azure_storage_account_config']['connection_string']
+        container_name    = container_name    or config['azure_storage_account_config']['container_name']
+        blob_name         = blob_name         or config['azure_storage_account_config']['blob_name']
+
     azure_params = {
-        "connection_string": config['azure_storage_account_config']['connection_string'],
-        "container_name": config['azure_storage_account_config']['container_name'],
-        "blob_name": config['azure_storage_account_config']['blob_name'],
+        "connection_string": connection_string,
+        "container_name":    container_name,
+        "blob_name":         blob_name,
     }
 
     data_ingestion = DataIngestionAzure(azure_params=azure_params, output_dir=RAW_DATA_DIR)
